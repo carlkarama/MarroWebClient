1efbc99f88027ed91950c8ee45d6a775b52a4664
Carl Karama <carlkarama4@gmail.com>
2024-01-10 20:15:05 +1100

    feat: connect searchbox to mock api

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,8 +1,15 @@
+import React, { useState } from 'react';
+import SearchResultList from '../components/SearchResultList/SearchResultList';
 import SearchBox from '../components/Search/SearchBox';
+import '../components/Search/SearchBox.css'
 
 const Search = () => {
+    const [results, setResults] = useState([]);
     return (
-        <SearchBox></SearchBox>
+        <div className="search-bar-container">
+            <SearchBox setResults={setResults}/>
+            <SearchResultList results={results}/>
+        </div>
     )
 }
 
