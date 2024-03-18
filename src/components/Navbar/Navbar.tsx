45ec5aeda74fdf0dac12037f66fe8845adb0d643
Carl Karama <carlkarama4@gmail.com>
2024-03-18 22:35:05 +1100

    feat: add Search, SearchResult and SearchResultList

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,4 +10,4 @@ const NavBar = () => {
   )
 }
 
-export default NavBar
+export default NavBar;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as NavBar } from './Navbar/Navbar';
 export { default as CTA } from './CTA/CTA';
 export { default as Feature } from './Feature/Feature';
+export { default as Search } from './Search/SearchBox';
+export { default as SearchResult } from './SearchResult/SearchResult';
+export { default as SearchResultList } from './SearchResultList/SearchResultList';
