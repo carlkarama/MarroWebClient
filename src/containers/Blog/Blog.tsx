5d7825b2156e63f632746ae45788c589874f5433
Carl Karama <carlkarama4@gmail.com>
2024-05-21 23:02:51 +1000

    feat: create blog template

--- src/containers/Blog/Blog.tsx
+++ src/containers/Blog/Blog.tsx
@@ -3,8 +3,21 @@ import './Blog.css';
 
 const WhatGPT3 = () => {
     return (
-        <div>
-            Blog
+        <div className="blog section_padding" id="blog">
+            <div className="blog-heading">
+                <h1 className="gradient_text">A lot is happening here</h1>
+            </div>
+            <div className="blog-container">
+                <div className="blog-container-group-a">
+                    { /* <Article /> */ }
+                </div>
+                <div className="blog-container-group-b">
+                { /* <Article /> */ }
+                { /* <Article /> */ }
+                { /* <Article /> */ }
+                { /* <Article /> */ }
+                </div>
+            </div>
         </div>
     )
 }
